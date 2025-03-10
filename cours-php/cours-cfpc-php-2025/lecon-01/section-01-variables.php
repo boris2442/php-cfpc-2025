89d50c7180c51c73f84707c0cbe6dd943fab1af1
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-10 15:36:03 +0100

    begin project

---
+++ cours-php/cours-cfpc-php-2025/lecon-01/section-01-variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    echo "hello word";
+    echo "<b>Laravel</b>";
+    echo "<h2>Welcome to Laravel</h2>";
+    ?>
+ijksjskksjskekjkwskjsjsjskjkqkjkakak
+    <h1>hello word</h1>
+</body>
+
+</html>
