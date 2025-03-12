5ac6d7fe555e61d0591cbd0b1483edd0975b1b99
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-12 15:56:43 +0100

    text

--- cours-php/cours-cfpc-php-2025/lecon-01/section-05-arrays.php
+++ cours-php/cours-cfpc-php-2025/lecon-01/section-05-arrays.php
@@ -1,3 +1,17 @@
 <?php
 //1 Les tableaux indexés!
+//Création d'un tableau de fruits :
+ $fruits = ["Mango", "Strawberry", "Blueberry"];
+
+  $personDetails = [
+ 'firstName' => 'Owen',
+ 'lastName' => 'Eva',
+ 'age' => 25,
+ 'occupation' => 'Developer'
+];
+foreach ($personDetails as $key => $value) {
+ echo "$key: $value<br>";
+}  
+    
+
 ?>
---
+++ cours-php/cours-cfpc-php-2025/lecon-01/section-06-loops.php
@@ -0,0 +1,33 @@
+<?php
+
+$fruits = ["Apple", "Banana", "Cherry"];
+
+foreach ($fruits as $index => $fruit) {
+    echo "Fruit $index:$fruit<br/>";
+}
+
+
+
+//2. Utilisation avec clé et valeur
+
+//foreach ($tableau as $cle => $valeur) {
+    // Code à exécuter
+//}
+
+// $utilisateur = [
+//     "nom" => "Boris",
+//     "email" => "boris@example.com",
+//     "âge" => 21
+// ];
+
+// foreach ($utilisateur as $cle => $valeur) {
+//     echo "$cle : $valeur <br>";
+// }
+
+
+$personDetails = [
+    "firstName" => "John",
+    "lastName" => "Doe",
+    "age" => 30,
+    "occupation" => "Developer"
+];
