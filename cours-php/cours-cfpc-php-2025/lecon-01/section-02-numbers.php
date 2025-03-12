9af2ffe7f184fa006fb5dd3594063bd6bc9677cb
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-12 14:49:05 +0100

    manipulations des strings in php

--- cours-php/cours-cfpc-php-2025/lecon-01/section-02-numbers.php
+++ cours-php/cours-cfpc-php-2025/lecon-01/section-02-numbers.php
@@ -2,6 +2,7 @@
 $a=5;
 $b=4;
 $c=40;
+$d=0;
 
 echo "addition".($a+$b) . "<br>";
 echo "Soustraction".($a-$b) . "<br>";
@@ -17,3 +18,12 @@ echo "Addition $a +$b";
 //fdiv permer de donner enormement de nouveaute
 
 echo "division flottante  avec fdiv:". fdiv(num1:$a, num2:$c).'<br>';
+
+//les operateurs d'incrementations
+$a=5;
+echo "Post-incrément (\$a++): " . $a++ . '<br>'; // Affiche 5, puis $a devient 6
+echo "Pré-incrément (++\$a): " . ++$a . '<br>'; // Incrémente à 7 puis affiche 7
+
+
+//les operateurs d'decrementations
+?>
