fb0f27bd549a7e7fdaeed8ef16c1d06e07891034
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-13 11:22:43 +0100

    debug coding

--- cours-php/cours-cfpc-php-2025/lecon-02/gestion-form.php
+++ cours-php/cours-cfpc-php-2025/lecon-02/gestion-form.php
@@ -1,17 +1,61 @@
 <?php
-
-
-
-
-
-
-
-
-
-
-
-
+if(isset($_POST['create'])){
+    echo "formulaire ok";
+    $nom_student=$_POST['nom'];
+    echo "nom: $nom_student";
+}
 
 
 
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./src/output.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1 class="text-3xl font-bold underline">
+        Hell
+    </h1>
+
+
+    <form action="" method="post" class="bg-white p-6 rounded shadow max-w-md mx-auto">
+        <div class="mb-4">
+            <input type="text" name="nom" placeholder="Nom"
+                class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
+        </div>
+        <div class="mb-4">
+            <input type="text" name="prenom" placeholder="Prénom"
+                class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
+        </div>
+        <div class="mb-4">
+            <input type="email" name="mail" placeholder="Email"
+                class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500">
+        </div>
+        <div class="text-center">
+            <input type="submit" name="create" value="Créer"
+                class="inline-block px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700">
+        </div>
+    </form>
+
+</body>
+
+</html>
