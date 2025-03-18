7248139bfc8c620a3c0aa6d893d62fac04d0a653
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-18 16:01:18 +0100

    debuggage

--- cours-php/cours-cfpc-php-2025/database/database.php
+++ cours-php/cours-cfpc-php-2025/database/database.php
@@ -1,17 +1,31 @@
 <?php
-$hostname="127.0.0.1";
-$username="root";
-$password="";
-$database="cours-cfpc-php-2025";
-$connect=new mysqli(hostname:$hostname, username:$username, password:$password, database:$database);
+// $hostname="127.0.0.1";
+// $username="root";
+// $password="";
+// $database="cours-cfpc-php-2025";
+// // $connect=new mysqli(hostname:$hostname, username:$username, password:$password, database:$database);
+// $connect=new PDO(hostname:$hostname, username:$username, password:$password, database:$database);
 
-if($connect->connect_error){
-    echo "Erreur:mauvaise connexion a la database";
-    $connect->connect_error;
-}else{
-    echo "Succes:connexion a la database reussie";
+// if($connect->connect_error){
+//     echo "Erreur:mauvaise connexion a la database";
+//     $connect->connect_error;
+// }else{
+//     echo "Succes:connexion a la database reussie";
+// }
+
+define("DBHOST", "localhost");
+define("DBUSER", "root");
+// define("DBUSER", "postgresql");
+define("DBPASS", "");
+define("DBNAME", "cours-cfpc-php-2025");
+
+// $dsn = "pgsql:dbname=" . DBNAME . "; host=" . DBHOST;
+$dsn = "mysql:dbname=" . DBNAME . "; host=" . DBHOST;
+try {
+    $db = new PDO($dsn, DBUSER, DBPASS);
+    $db->exec("SET NAMES utf8");
+   
+} catch (PDOException $e) {
+    die($e->getMessage());
+  
 }
-
-
-
-?>
--- cours-php/cours-cfpc-php-2025/database/index_student.php
+++ cours-php/cours-cfpc-php-2025/database/index_student.php
@@ -2,34 +2,37 @@
 require_once "database.php";
 
 $sql = "SELECT* FROM `student`";
-$requete = $connect->query($sql);
+// $requete = $connect->query($sql);
+// $requete = $db->query($sql);
+$requete = $db->prepare($sql);
+
+$requete->execute();
+$result = $requete->fetchAll();
 echo "<pre>";
-var_dump(value: $requete);
+var_dump(value: $result);
 echo "<pre>";
 
-foreach ($requete as $row) {
-    echo " id:" . $row['id'] . " <br>";
-    echo " Nom:" . $row['name'] . " <br>";
-    echo " prenom:" . $row['prenom'] . " <br>";
-    echo " mail:" . $row['mail'] . " <br>";
-    echo " password:" . $row['password'] . " <br>";
+
+if ($requete->rowCount() > 0) {
+// if ($result->rowCount() > 0) {
+    // if ($requete->rowCount()> 0) {
+
+
+
+    foreach ($requete as $row) {
+    // foreach ($result as $row) {
+        echo " id:" . $row['id'] . " <br>";
+        echo " Nom:" . $row['name'] . " <br>";
+        echo " prenom:" . $row['prenom'] . " <br>";
+        echo " mail:" . $row['mail'] . " <br>";
+        echo " password:" . $row['password'] . " <br>";
+    }
+} else {
+    echo "Aucun etudiant trouvée dans la database";
 }
 
 
-
-
-
-
-
-
-
-
 ?>
-
-
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
