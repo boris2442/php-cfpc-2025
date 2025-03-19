5be9024ac108517fa85877eb7bdce38f45092779
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-19 14:13:43 +0100

    debuggage

--- cours-php/cours-cfpc-php-2025/tp-01-crud-student/database.php
+++ cours-php/cours-cfpc-php-2025/tp-01-crud-student/database.php
@@ -9,9 +9,10 @@ define("DBNAME", "tp-01-crud-student");
 $dsn = "mysql:dbname=" . DBNAME . "; host=" . DBHOST;
 try {
     $db = new PDO($dsn, DBUSER, DBPASS);
-    // $db->setAttribute(attribute:PDO::FETCH_ASSOC);
+   
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->exec("SET NAMES utf8");
-    echo "connexion a la database reussie";
+    // echo "connexion a la database reussie";
    
 } catch (PDOException $e) {
     die($e->getMessage());
