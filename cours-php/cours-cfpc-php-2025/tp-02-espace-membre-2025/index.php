7f61abd859eca47634b757ba3ee800adda60052b
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-27 14:01:37 +0100

    text

--- cours-php/cours-cfpc-php-2025/tp-02-espace-membre-2025/index.php
+++ cours-php/cours-cfpc-php-2025/tp-02-espace-membre-2025/index.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // return $data_result;
         return (htmlspecialchars(stripslashes(trim($data))));
     }
-    
+
     //recuperation des donnees du formulaires
     $pseudo = htmlspecialchars($_POST['pseudo']);
     $mail = htmlspecialchars($_POST['mail']);
@@ -77,63 +77,68 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 require_once "header-and-footer/header.php";
 ?>
 
-<div align="center">
-    <h2 class="text-4xl font-bold text-green-900 text-center mb-6">Veuillez vous inscrire</h2>
 
-    <form method="POST" action="" class="bg-white p-6 rounded shadow max-w-lg mx-auto">
-        <div class="flex flex-col gap-[7px] pt-[7px]">
-            <?php
-            if (isset($error)) {
-                echo '<p class="bg-red-500 w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500 text-white font-bold">' . $error . '</p>';
-            }
+<h2 class="text-4xl font-bold text-green-900 text-center mb-6">Veuillez vous inscrire</h2>
 
-            ?>
-            <div class="text-left flex flex-col gap-[7px]">
-                <label for="pseudo" class="">Pseudo :</label>
+<form method="POST" action="" class="bg-white p-6 rounded shadow max-w-lg mx-auto">
+    <div class="flex flex-col gap-[7px] pt-[7px]">
+        <?php
+        if (isset($error)) {
+            echo '<p class="bg-red-500 w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500 text-white font-bold">' . $error . '</p>';
+        }
 
-                <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo"  value="<?php echo $pseudo ?? ""  ?>"    class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500"  />
-            </div>
-            <div class="text-left flex flex-col gap-[7px]">
-                <label for="mail">Mail :</label>
+        ?>
+        <div class="text-left flex flex-col gap-[7px]">
+            <label for="pseudo" class="">Pseudo :</label>
 
-                <input type="text" placeholder="Votre mail" id="mail"   value="<?php echo $mail ?? ""  ?>"    name="mail" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
-            </div>
-            <div class="text-left flex flex-col gap-[7px]">
-                <label for="mail2">Confirmation du mail :</label>
+            <input type="text" placeholder="Votre pseudo" id="pseudo" name="pseudo" value="<?php echo $pseudo ?? ""  ?>" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
+        </div>
+        <div class="text-left flex flex-col gap-[7px]">
+            <label for="mail">Mail :</label>
 
-                <input type="text"    value="<?php echo $mail2 ?? ""  ?>"     placeholder="Confirmez votre mail" id="mail2" name="mail2" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
-            </div>
-            <!-- <div class="text-left flex flex-col gap-[7px]">
+            <input type="text" placeholder="Votre mail" id="mail" value="<?php echo $mail ?? ""  ?>" name="mail" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
+        </div>
+        <div class="text-left flex flex-col gap-[7px]">
+            <label for="mail2">Confirmation du mail :</label>
+
+            <input type="text" value="<?php echo $mail2 ?? ""  ?>" placeholder="Confirmez votre mail" id="mail2" name="mail2" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
+        </div>
+        <!-- <div class="text-left flex flex-col gap-[7px]">
                 <label for="mdp">Mot de passe :</label>
 
                 <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
             </div> -->
-            <i class="far fa-facebook"></i>
-            <div class="text-left flex flex-col gap-[7px]">
-    <label for="mdp">Mot de passe :</label>
-    <div class="relative">
-        <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp"
-            class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500"  value="<?php echo $mdp ?? ""  ?>"        />
-        <!-- <i class="fa fa-eye absolute right-3 top-3 cursor-pointer text-gray-500" id="togglePassword"></i> -->
-        <!-- <i class="far fa-eye absolute right-3 top-3 cursor-pointer text-gray-500" id="togglePassword"></i> -->
-        <i class="fa-solid fa-eye  absolute right-3 top-3 cursor-pointer text-gray-500"  id="togglePassword"></i>
-    </div>
-</div>
-            <div class="text-left flex flex-col gap-[7px]">
-                <label for="mdp2">Confirmation du mot de passe :</label>
-
-                <input type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500"  value="<?php echo $mdp2 ?? ""  ?> "   />
-            </div>
-            <div class="text-left flex flex-col gap-[7px]">
-                <input type="submit" name="forminscription" value="S'inscrire" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500 bg-green-100" />
+        <!-- <i class="far fa-facebook"></i> -->
+        <div class="text-left flex flex-col gap-[7px]">
+            <label for="mdp">Mot de passe :</label>
+            <div class="relative">
+                <input type="password" placeholder="Votre mot de passe" id="mdp" name="mdp"
+                    class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" value="<?php echo $mdp ?? ""  ?>" />
+                <!-- <i class="fa fa-eye absolute right-3 top-3 cursor-pointer text-gray-500" id="togglePassword"></i> -->
+                <!-- <i class="far fa-eye absolute right-3 top-3 cursor-pointer text-gray-500" id="togglePassword"></i> -->
+                <i class="fa-solid fa-eye  absolute right-3 top-3 cursor-pointer text-gray-500" id="togglePassword"></i>
             </div>
         </div>
-        Déjà un compte ?<a
-            href="connexion.php">Se connecter</a>
+        <div class="text-left flex flex-col gap-[7px]">
+            <label for="mdp2">Confirmation du mot de passe :</label>
 
-    </form>
+            <input type="password" placeholder="Confirmez votre mdp" id="mdp2" name="mdp2" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" value="<?php echo $mdp2 ?? ""  ?> " />
+        </div>
+        <div class="text-left flex flex-col gap-[7px]">
+            <input type="submit" name="forminscription" value="S'inscrire" class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500 bg-green-100" />
+        </div>
 
-</div>
+        <div class="w-full text-left flex  gap-[7px] justify-between">
+            <small class="border border-green-300 p-2 rounded focus:outline-none focus:border-green-500 bg-green-100">Déjà un compte ? </small>
+
+            <a
+                href="connexion.php" class="border border-green-300 p-2 rounded focus:outline-none focus:border-green-500 bg-green-100">Se connecter
+            </a>
+        </div>
+    </div>
+</form>
+
+
 <script src="./javascript/script-index.js"></script>
 <?php
 require_once "./header-and-footer/footer.php";
