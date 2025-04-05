c9502d46c8d8d27ba7c35351b2e90e4e9f8b46b5
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-05 12:51:11 +0200

    debugage

--- cours-php/cours-cfpc-php-2025/Espace_membre_2025_tp_06_procedurale_php/edit_article.php
+++ cours-php/cours-cfpc-php-2025/Espace_membre_2025_tp_06_procedurale_php/edit_article.php
@@ -1,6 +1,51 @@
 <?php
 require_once "database.php";
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM `articles2` WHERE id=:id";
+    $requete = $db->prepare($sql);
+    $requete->bindValue(':id', $id);
+    $requete->execute();
+    $result = $requete->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
+        $article_title = $result['title'];
+        $author_article = $result['author'];
+        $article_content = $result['content'];
+    } else {
+        echo "Aucun article trouvé.";
+    }
+} else {
+    echo "Aucun identifiant trouvé.";
+}
+
+if(isset($_POST['submit_article'])){
+    $article_title = $_POST['article_title'];
+    $author_article = $_POST['author_article'];
+    $article_content = $_POST['article_content'];
+    $error ="";
+    if(empty($article_title) || empty($author_article) || empty($article_content)){
+        $error = "Veuillez remplir tous les champs";
+    }else{
+        $sql = "UPDATE `articles2` SET `title`=:article_title, `author`=:author_article, `content`=:article_content WHERE id=:id";
+        $requete = $db->prepare($sql);
+        $requete->bindValue(':id', $id);
+        $requete->bindValue(':article_title', $article_title);
+        $requete->bindValue(':author_article', $author_article);
+        $requete->bindValue(':article_content', $article_content);
+        if($requete->execute()){
+            header("Location:ajout_article.php");
+            exit();
+        }else{
+            echo "Erreur lors de la mise à jour de l'article.";
+        }
+    }
+}
+// if(isset($_POST['submit_article'])){
+
+
+
+
 ?>
 
 
@@ -20,11 +65,11 @@ require_once "header-and-footer/header.php";
 
         <div class="text-left flex flex-col gap-[7px]">
             <label for="title">Titre de l'article</label>
-            <input type="text" placeholder="Title of article" id="title" name="article_title" value="" required class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
+            <input type="text" placeholder="Title of article" id="title" name="article_title" value="<?=   $article_title?>" required class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
         </div>
         <div class="text-left flex flex-col gap-[7px]">
             <label for="auteur">Auteur de l'aticle</label>
-            <input type="text" placeholder="Boris La menace" id="auteur" name="author_article" value="" required class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
+            <input type="text" placeholder="Boris La menace" id="auteur" name="author_article" value="<?=$author_article?>" required class="w-full border border-green-300 p-2 rounded focus:outline-none focus:border-green-500" />
         </div>
 
         <div class="text-left flex flex-col gap-[7px]">
