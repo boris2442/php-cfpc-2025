245f699b966b7cdcbc2f95ddc1b322f51661ba26
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-08 12:33:21 +0200

    text

--- cours-php/gestion_compte_user_cfpc_2025/register.php
+++ cours-php/gestion_compte_user_cfpc_2025/register.php
@@ -1,21 +1,54 @@
 <?php
 session_start();
 require_once('./includes/database.php');
-if($_POST){
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+require_once('./includes/clean_input.php');
+if ($_POST) {
+    echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";
 
 
-///////////////////username
+    ///////////////////username
 
-//recuperation des erreurs sous forme de tableau
-$errors = [];
-if(empty($_POST['username']) || !preg_match("/^[a-zA-Z0-9_]{3,20}$/", $_POST['username']) ){
-    $errors['username'] = "Le nom  doit contenir entre 3 et 20 caractères alphanumériques";
-    var_dump($errors['username']);
-}
+    //recuperation des erreurs sous forme de tableau
+    $errors = [];
+    if (empty($_POST['username']) || !preg_match("/^[a-zA-Z0-9_]{3,20}$/", $_POST['username'])) {
+        $errors['username'] = "Le nom  doit contenir entre 3 et 20 caractères alphanumériques";
+        var_dump($errors['username']);
+    } else {
+        $username = htmlspecialchars(trim($_POST['username']));
+        $stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
+        $stmt->execute([$username]);
+        if ($stmt->rowCount() > 0) {
+            $errors['username'] = "Ce nom d'utilisateur existe déjà";
+            var_dump($errors['username']);
+        }
+    }
+    //email
+    if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "L'email est invalide";
+    } else {
+        $email = htmlspecialchars(trim($_POST['email']));
+        $stmt = $db->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        if ($stmt->rowCount() > 0) {
+            $errors['email'] = "Cet email existe déjà";
+        }
+    }
 
+    ////password
+
+    if (
+        empty($_POST['password']) ||
+        !preg_match(
+            "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/",
+            $_POST['password']
+        )
+    ) {
+        $errors['password'] = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre";
+    } elseif ($_POST['password'] !== $_POST['confirm_password']) {
+        $errors['confirm_password'] = "Les mots de passe ne correspondent pas";
+    }
 }
 
 
@@ -32,25 +65,25 @@ if(empty($_POST['username']) || !preg_match("/^[a-zA-Z0-9_]{3,20}$/", $_POST['us
         <div class="header">
             <h2>S'inscrire</h2>
         </div>
-     
 
-     <?php
-if (!empty($errors)) {
-    echo '<div style="color:white; text-align: center; background-color:#ff6c6c;padding:2px 7px; margin-bottom:10px; font-size:23px;">' . reset($errors) . '</div>';
-}
-?>
+
+        <?php
+        if (!empty($errors)) {
+            echo '<div style="color:white; text-align: center; background-color:#ff6c6c;padding:2px 7px; margin-bottom:10px; font-size:16px;">' . reset($errors) . '</div>';
+        }
+        ?>
         <form action="" class="form" id="form" method="post" enctype="multipart/form-data">
             <div class="form-control">
                 <label for="username">Nom d'utilisateur</label>
                 <input type="text" id="username" placeholder="rostodev" name="username" autocomplete="off"
-                    value="">
+                    value="<?= isset($_POST['username']) ? $_POST['username'] : ''; ?>">
 
             </div>
 
             <div class="form-control">
                 <label for="email">Email</label>
-                <input type="email" id="email" placeholder="rostodev@gmail.com" name="email" 
-                value="">
+                <input type="email" id="email" placeholder="rostodev@gmail.com" name="email"
+                    value="<?= isset($_POST['email']) ? clean_input($_POST['email']) : ''; ?>" autocomplete="off">
             </div>
 
             <div class="form-control">
@@ -64,7 +97,7 @@ if (!empty($errors)) {
                 <input type="password" id="confirm_password" name="confirm_password">
 
             </div>
-            
+
 
             <button type="submit"> S'inscrire</button>
 
