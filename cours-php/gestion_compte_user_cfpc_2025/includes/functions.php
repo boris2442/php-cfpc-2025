8a34ae05cded56d9edab797d0b9c9a276f77bb1c
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-10 11:38:24 +0200

    text

---
+++ cours-php/gestion_compte_user_cfpc_2025/includes/functions.php
@@ -0,0 +1,15 @@
+<?php
+function generateToken($length){
+     $alphanum=array_merge(range(0, 9), range('a', 'z'), range('A', 'Z')); 
+    $alphanumstring = implode('', $alphanum); 
+    return substr(str_shuffle(str_repeat($alphanumstring, $length)), 0,  $length); 
+}
+//void ne retourne rien
+/**
+ * fonction qui permet de generer une fonction de facon alestoire
+ * @param int $length
+ * @return string
+ */
+
+
+?>
--- cours-php/gestion_compte_user_cfpc_2025/register.php
+++ cours-php/gestion_compte_user_cfpc_2025/register.php
@@ -2,26 +2,61 @@
 session_start();
 require_once('./includes/database.php');
 require_once('./includes/clean_input.php');
+require_once('./includes/functions.php');
+
+// $chiffre = range(0, 9); // 1.cree un tableau contenant les chiffes de 0 a 9
+// $minuscule = range('a', 'z'); //2. creent un tableau contenant  tous les lettres de a a a z en minuscules
+// $majuscule = range('A', 'Z'); //3. creent un tableau contenant  tous les lettres de a a a z en majuscules
+// // var_dump($chiffre);
+// // $alphanum=array_merge($chiffre,$minuscule,$majuscule);
+// $alphanum = array_merge(range(0, 9), range('a', 'z'), range('A', 'Z')); //4.cette methode permet de fusionner les tableaux pour creer un tableau alphanumerique
+//fusionne
+// $alphanumstring = implode('', $alphanum); 
+//5.concatene les elements en une chaine de caractere
+//6 La methode str_repeat permet de repeter les elements 
+//  $token1=str_shuffle(str_repeat($alphanumstring, 100)); 
+//str
+//  $token2=substr(str_shuffle(str_repeat($alphanumstring, 100)), 0, 100); 
+//la methode substr permet de retourner les valeurs d'un tableau 
+// echo '<pre>';
+// print_r($chiffre);
+// print_r($minuscule);
+// print_r($majuscule);
+// print_r($alphanum)
+// echo '</pre>';
+
+// $token = generateToken(100);
+
+// echo '<pre>';
+// print_r($token);
+// // print_r($alphanum);
+// // print_r($alphanumstring);
+// // print_r(str_repeat($alphanumstring, 100));
+// // print_r( $token2);
+// echo '</pre>';
+// die();
 if ($_POST) {
     echo "<pre>";
-    var_dump($_POST);
+    // var_dump($_POST);
     echo "</pre>";
 
 
-    
+
 
     //recuperation des erreurs sous forme de tableau
     $errors = [];
-    if (empty($_POST['username']) || !preg_match("/^[a-zA-Z0-9_]{3,20}$/", $_POST['username'])) {
+    if (empty($_POST['username'])
+          || !preg_match("/^[a-zA-Z0-9_]{3,20}$/", $_POST['username'])
+    ) {
         $errors['username'] = "Le nom  doit contenir entre 3 et 20 caractères alphanumériques";
-        var_dump($errors['username']);
+        // var_dump($errors['username']);
     } else {
         $username = htmlspecialchars(trim($_POST['username']));
         $stmt = $db->prepare("SELECT * FROM users WHERE username = ?");
         $stmt->execute([$username]);
         if ($stmt->rowCount() > 0) {
             $errors['username'] = "Ce nom d'utilisateur existe déjà";
-            var_dump($errors['username']);
+            // var_dump($errors['username']);
         }
     }
     //email
@@ -53,11 +88,12 @@ if ($_POST) {
 
 
     if (
-        empty($_POST['password']) ||
-        !preg_match(
-            "/[a-zA-Z0-9_\/]{8,}$/",
-            $_POST['password']
-        )
+        empty($_POST['password'])
+        //  ||
+        // !preg_match(
+        //     "/[a-zA-Z0-9_\/]{8,}$/",
+        //     $_POST['password']
+        // )
     ) {
         $errors['password'] = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule et un chiffre";
     } elseif (trim($_POST['password']) !== trim($_POST['confirm_password'])) {
@@ -66,13 +102,35 @@ if ($_POST) {
     //INSERT INTO
     if (empty($errors)) {
         $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $stmt = $db->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-        $stmt->execute([$username, $email, $password]);
+        //appele la fonction generateToken pour generer un token aleatoire de 100 caracteres
+        $token = generateToken(100);
+        var_dump($token);
+        // die();
+        $stmt = $db->prepare("INSERT INTO users (username, email, password, confirmation_token) VALUES (:username, :email, :password, :confirmation_token)");
+        $stmt->execute([
+            'username' => $username,
+            'email' => $email,
+            'password' => $password,
+            'confirmation_token' => $token
+        ]);
+        // var_dump(  $stmt);
+        // die();
         // $_SESSION['success'] = "Inscription réussie";
-        header('location:login.php');
-        exit();
+        // header('location:login.php');
+        // exit();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
